7b325222a53eff8e41ddf7ea365ce2d6ea78135f
Evgeny Berg <berg.evg.1@gmail.com>
2022-12-23 18:19:11 +0400

    feat: grid

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,10 +1,42 @@
 import init, { Grid } from "rust-snake"
 
+const CELL_SIZE = 50;
+
 const canvas = document.getElementById('canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
 init().then(() => {
     const grid = Grid.new(8, 9);
 
-    console.log(grid.getWidth(), grid.getHeight());
+    canvas.width = CELL_SIZE * grid.getWidth();
+    canvas.height = CELL_SIZE * grid.getHeight();
+
+    drawGrid(grid);
 })
+
+const drawGrid = (grid: Grid) => {
+    if (!ctx) {
+        console.error('Can\'t find canvas');
+        return
+    }
+
+    const canvasWidth = canvas.width,
+        canvasHeight = canvas.height,
+        countOfColumns = grid.getWidth(),
+        countOfRows = grid.getHeight();
+
+
+    ctx.beginPath();
+
+    for (let x = 0; x < countOfColumns; x++) {
+        ctx.moveTo(CELL_SIZE * x, 0);
+        ctx.lineTo(CELL_SIZE * x, canvasWidth);
+    }
+
+    for (let y = 0; y < countOfRows; y++) {
+        ctx.moveTo(0, CELL_SIZE * y);
+        ctx.lineTo(canvasHeight, CELL_SIZE * y);
+    }
+
+    ctx.stroke();
+}
