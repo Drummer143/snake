e6a946a7f939ceee5e29b7633c721d8c2a870f10
Evgeny Berg <berg.evg.1@gmail.com>
2022-12-24 00:24:16 +0400

    feat: made snake head & partically snake moving

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,17 +1,27 @@
-import init, { Grid } from "rust-snake"
+import init, { Grid, Snake } from "rust-snake"
+
+type Coordinates = {
+    x: number
+    y: number
+}
 
 const CELL_SIZE = 50;
+const SNAKE_PADDING = 0.05;
 
 const canvas = document.getElementById('canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
 init().then(() => {
-    const grid = Grid.new(8, 9);
+    const grid = Grid.new(9, 9);
+    const snake = Snake.new(grid);
 
-    canvas.width = CELL_SIZE * grid.getWidth();
-    canvas.height = CELL_SIZE * grid.getHeight();
+    canvas.width = CELL_SIZE * grid.get_width();
+    canvas.height = CELL_SIZE * grid.get_height();
 
     drawGrid(grid);
+    drawSnake(snake);
+
+    play(snake, grid);
 })
 
 const drawGrid = (grid: Grid) => {
@@ -20,23 +30,66 @@ const drawGrid = (grid: Grid) => {
         return
     }
 
+    debugger
     const canvasWidth = canvas.width,
         canvasHeight = canvas.height,
-        countOfColumns = grid.getWidth(),
-        countOfRows = grid.getHeight();
+        countOfColumns = grid.get_width(),
+        countOfRows = grid.get_height();
 
-
+    ctx.strokeStyle = '#444'
     ctx.beginPath();
 
-    for (let x = 0; x < countOfColumns; x++) {
+    for (let x = 0; x < countOfColumns + 1; x++) {
         ctx.moveTo(CELL_SIZE * x, 0);
-        ctx.lineTo(CELL_SIZE * x, canvasWidth);
+        ctx.lineTo(CELL_SIZE * x, canvasHeight);
     }
 
-    for (let y = 0; y < countOfRows; y++) {
+    for (let y = 0; y < countOfRows + 1; y++) {
         ctx.moveTo(0, CELL_SIZE * y);
-        ctx.lineTo(canvasHeight, CELL_SIZE * y);
+        ctx.lineTo(canvasWidth, CELL_SIZE * y);
     }
 
     ctx.stroke();
+}
+
+const drawSnake = (snake: Snake) => {
+    const head = convertStringCoordsToArray(snake.get_head_coordinates());
+
+    drawHead(head);
+}
+
+const drawHead = ({ x, y }: Coordinates) => {
+    if (!ctx) { return; }
+
+    ctx.fillStyle = 'green';
+    ctx.beginPath();
+
+    ctx.moveTo((x + SNAKE_PADDING) * CELL_SIZE, (y + 1) * CELL_SIZE);
+    ctx.lineTo((x + SNAKE_PADDING) * CELL_SIZE, (y + 0.5 + SNAKE_PADDING) * CELL_SIZE)
+    ctx.lineTo((x + 0.5) * CELL_SIZE, (y + SNAKE_PADDING * 2) * CELL_SIZE)
+    ctx.lineTo((x + 1 - SNAKE_PADDING) * CELL_SIZE, (y + 0.5 + SNAKE_PADDING) * CELL_SIZE)
+    ctx.lineTo((x + 1 - SNAKE_PADDING) * CELL_SIZE, (y + 1) * CELL_SIZE)
+
+    ctx.stroke();
+    ctx.fill();
+}
+
+const convertStringCoordsToArray = (stringCoords: string): Coordinates => {
+    const [x, y] = stringCoords.split(' ').map(coord => +coord);
+
+    return { x, y };
+}
+
+const play = (snake: Snake, grid: Grid) => {
+    document.addEventListener('keydown', e => {
+        if (e.shiftKey && e.code === 'KeyX') {
+            snake.move_snake();
+
+            ctx?.clearRect(0, 0, canvas.width, canvas.height)
+            drawSnake(snake);
+            drawGrid(grid);
+        } else if (e.code === 'KeyC') {
+            console.log(snake.get_head_coordinates());
+        }
+    })
 }
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,14 +1,112 @@
 use wasm_bindgen::prelude::*;
 
 #[global_allocator]
-static  ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-pub fn greet(message: &str) {
-    alert(message);
+pub struct Grid {
+    width: usize,
+    height: usize,
 }
 
 #[wasm_bindgen]
-extern {
-    pub fn alert(s: &str);
+impl Grid {
+    pub fn new(width: usize, height: usize) -> Grid {
+        Grid { width, height }
+    }
+
+    pub fn get_width(&self) -> usize {
+        return self.width;
+    }
+
+    pub fn get_height(&self) -> usize {
+        return self.height;
+    }
+}
+
+#[wasm_bindgen]
+pub struct SnakeCell {
+    pub x: isize,
+    pub y: isize,
+}
+
+#[wasm_bindgen]
+pub struct Orientation {
+    pub x: isize,
+    pub y: isize,
+}
+
+#[wasm_bindgen]
+pub struct Snake {
+    max_size: usize,
+    cells: Vec<SnakeCell>,
+    grid: Grid,
+    orientation: Orientation,
+}
+
+#[wasm_bindgen]
+impl Snake {
+    pub fn new(grid: Grid) -> Snake {
+        let rows = grid.get_height();
+        let columns = grid.get_width();
+
+        Snake {
+            grid,
+            max_size: rows * columns,
+            cells: vec![SnakeCell {
+                x: (columns / 2) as isize,
+                y: (rows / 2) as isize,
+            }],
+            orientation: Orientation { x: 0, y: 1 },
+        }
+    }
+
+    pub fn check_size(&self) -> bool {
+        return self.max_size > self.cells.len();
+    }
+
+    pub fn check_collision_with_walls(&self) -> bool {
+        return self.cells[0].x < 0
+            || self.cells[0].x > self.grid.get_width() as isize
+            || self.cells[0].y < 0
+            || self.cells[0].y > self.grid.get_height() as isize;
+    }
+
+    pub fn check_collision_with_self(&self) -> bool {
+        let mut is_collision = false;
+
+        for i in 1..self.cells.len() {
+            if self.cells[i].x == self.cells[0].x && self.cells[i].y == self.cells[0].y {
+                is_collision = true;
+                break;
+            }
+        }
+
+        return is_collision;
+    }
+
+    pub fn get_head_coordinates(&self) -> String {
+        let head = &self.cells[0];
+
+        return format!("{} {}", head.x, head.y);
+    }
+
+    pub fn get_orientation(&self) -> String {
+        let orientation = &self.orientation;
+
+        return format!("{} {}", orientation.x, orientation.y);
+    }
+
+    pub fn move_snake(&mut self) {
+        let orientation = &self.orientation;
+
+        for i in 0..self.cells.len() {
+            self.cells[i].x += orientation.x;
+            self.cells[i].y -= orientation.y;
+        }
+    }
+
+    // pub fn lol() -> String {
+    //     return format!("test");
+    // }
 }
